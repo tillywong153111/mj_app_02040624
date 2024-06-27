671bf521f18680bf8f855c1b7d1d33517f0c2f48
Tillywong <tillywong15@gmail.com>
2024-06-27 13:03:48 +0800

    修改了生产环境下的路径，引用了git的url导入wechat图

--- src/preload/index.js
+++ src/preload/index.js
@@ -4,7 +4,6 @@ import path from 'path';
 import { pathToFileURL } from 'url'; // 引入 url 模块的 pathToFileURL 方法
 import { app } from 'electron';
 
-
 console.log('预加载脚本加载');
 
 // 假设在开发环境中，assets 目录位于项目根目录下，而在生产环境中，它位于 resources/app.asar 下
@@ -12,20 +11,14 @@ const isDevelopment = process.env.NODE_ENV === 'development' || !process.default
 
 const api = {
   getImagePath: () => {
-    let imagePath;
     if (isDevelopment) {
-      // 假设assets目录位于项目根目录下
-      imagePath = path.join(__dirname, '../../../assets/wechat.jpg');
+      // 开发环境 - 使用本地路径
+      const imagePath = path.join(__dirname, '../../../assets/wechat.jpg');
+      return pathToFileURL(imagePath).href;
     } else {
-       // 生产环境下，资源位于app.asar内或用户数据目录下
-      // 如果你决定把资源放在用户数据目录下，确保你的主进程或者打包脚本已经相应地复制了这些资源
-      const userDataPath = app.getPath('userData'); // 使用Electron的app模块获取用户数据目录的路径
-      // imagePath = path.join(userDataPath, 'assets', 'wechat.jpg');
-      // 如果资源直接包含在 app.asar 中，使用下面的路径
-      imagePath = path.join(process.resourcesPath, 'app.asar', 'assets', 'wechat.jpg');
+      // 生产环境 - 使用 GitHub 上托管的图片的 URL
+      return "https://raw.githubusercontent.com/tillywong153111/mj_resources/main/wechat.jpg";
     }
-    // 使用 pathToFileURL 转换 imagePath 为 file:// URL
-    return pathToFileURL(imagePath).href;
   }
 };
 
